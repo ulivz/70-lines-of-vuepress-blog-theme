ff5c5354ffb915e378ed7e0de33ad3639be4af20
ULIVZ <472590061@qq.com>
2019-06-04 03:30:56 +0800

    feat: init

---
+++ examples/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '70 lines of vuepress blog theme',
+  theme: require.resolve('../../')
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: [
+    ['@vuepress/blog', {
+      directories: [
+        {
+          id: 'post',
+          dirname: '_posts',
+          path: '/',
+          pagination: {
+            perPagePosts: 2,
+          },
+        },
+      ],
+      frontmatters: [
+        {
+          id: "tag",
+          keys: ['tag', 'tags'],
+          path: '/tag/',
+          layout: 'Tag',
+          frontmatter: { title: 'Tag' },
+          itemlayout: 'Tag',
+          pagination: {
+            perPagePosts: 2
+          }
+        },
+      ]
+    }],
+  ],
+}
